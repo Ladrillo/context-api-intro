c01988671ef56696e2305005ed25cb977029cd80
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-02-20 11:42:25 +0100

    wip

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,10 +1,13 @@
 import React, { useContext } from 'react'
 import playerContext from "../contexts/player"
+import { themeContext } from '../contexts/theme'
+
 
 export default function Controls() {
   const { actions } = useContext(playerContext)
+  const stuff = useContext(themeContext)
   return (
-    <div className='Controls'>
+    <div className='Controls' style={{ ...stuff }}>
       {
         Object.keys(actions).map(actionName => {
           return (
